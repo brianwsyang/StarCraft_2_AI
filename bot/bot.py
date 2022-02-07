2f3ac8c7aee03912ed647b5e0ce44a8b4756be57
Brian Yang <brianwsyang@berkeley.edu>
2022-02-07 15:38:53 -0800

    2022.02.07.v3: training basic protoss unit

--- bot/bot.py
+++ bot/bot.py
@@ -31,6 +31,9 @@ class CompetitiveBot(BotAI):
 
     async def on_step(self, iteration):
         # Populate this function with whatever your bot should do!
+        if iteration == 0:
+            await self.chat_send("GLHF")
+        
         await self.distribute_workers()
         await self.build_supply()
         await self.build_workers()
@@ -39,8 +42,15 @@ class CompetitiveBot(BotAI):
         await self.build_gas()
         await self.build_cybernetic()
         await self.train_stalkers()
+        if self.structures(UnitTypeId.CYBERNETICSCORE).ready:
+            await self.rsrch_warpgate()
+            await self.build_four_gw()
+        await self.chrono_boost()
 
-        pass
+        if self.time > 400:
+            print(self.time)
+            self.chat_send("GG")
+            self.leave()
     
     async def build_workers(self):
         main = self.townhalls.ready.random
@@ -53,7 +63,12 @@ class CompetitiveBot(BotAI):
         if ( self.supply_left < 3 and 
                 not self.already_pending(UnitTypeId.PYLON) and 
                 self.can_afford(UnitTypeId.PYLON) ):
-            await self.build(UnitTypeId.PYLON, near=list(self.main_base_ramp.upper)[0])
+            if self.structures(UnitTypeId.PYLON).amount < 1:
+                await self.build(UnitTypeId.PYLON, near=list(self.main_base_ramp.upper)[0])
+            if self.structures(UnitTypeId.PYLON).amount > 3:
+                await self.build(UnitTypeId.PYLON, near=self.townhalls.ready.random.position.towards(list(self.main_base_ramp.upper)[0], -10))
+            else:
+                await self.build(UnitTypeId.PYLON, near=self.townhalls.ready.random.position.towards(list(self.main_base_ramp.upper)[0], 5))
     
     async def build_gateway(self):
         if ( self.structures(UnitTypeId.PYLON).ready and
@@ -83,15 +98,51 @@ class CompetitiveBot(BotAI):
                 if not self.structures(UnitTypeId.CYBERNETICSCORE):
                     if ( self.can_afford(UnitTypeId.CYBERNETICSCORE) and
                             self.already_pending(UnitTypeId.CYBERNETICSCORE) == 0 ):
-                        await self.build(UnitTypeId.CYBERNETICSCORE, near=pylon)
+                        await self.build(UnitTypeId.CYBERNETICSCORE, near=pylon.position.towards(self.townhalls.ready.random, 10))
     
+    async def build_four_gw(self):
+        if ( self.structures(UnitTypeId.PYLON).ready and
+                self.can_afford(UnitTypeId.GATEWAY) and
+                self.structures(UnitTypeId.GATEWAY).amount + self.structures(UnitTypeId.WARPGATE).amount < 4 ):
+            pylon = self.structures(UnitTypeId.PYLON).ready.random
+            await self.build(UnitTypeId.GATEWAY, near=pylon)
+        if ( self.structures(UnitTypeId.PYLON).ready and
+                self.can_afford(UnitTypeId.GATEWAY) and
+                self.structures(UnitTypeId.GATEWAY).amount + self.structures(UnitTypeId.WARPGATE).amount < 4 ):
+            await self.build(UnitTypeId.GATEWAY, near=pylon)
+        if ( self.structures(UnitTypeId.PYLON).ready and
+                self.can_afford(UnitTypeId.GATEWAY) and
+                self.structures(UnitTypeId.GATEWAY).amount + self.structures(UnitTypeId.WARPGATE).amount < 4 ):
+            await self.build(UnitTypeId.GATEWAY, near=pylon)
+
+    async def chrono_boost(self):
+        if self.structures(UnitTypeId.PYLON):
+            main = self.townhalls.ready.random
+            if ( not main.is_idle ):
+                main(AbilityId.EFFECT_CHRONOBOOSTENERGYCOST, main)
+    
+    async def rsrch_warpgate(self):
+        if ( self.can_afford(AbilityId.RESEARCH_WARPGATE) and
+                self.already_pending_upgrade(UpgradeId.WARPGATERESEARCH) == 0 ):
+            cybercore = self.structures(UnitTypeId.CYBERNETICSCORE).ready.first
+            cybercore.research(UpgradeId.WARPGATERESEARCH)
+
+
     async def train_stalkers(self):
         for gateway in self.structures(UnitTypeId.GATEWAY).ready:
             if ( self.can_afford(UnitTypeId.STALKER) and
                     gateway.is_idle ):
-                    gateway.train(UnitTypeId.STALKER)
+                gateway.train(UnitTypeId.STALKER)
+                # unit.move(self.main_base_ramp.bottom_center)
+    
+    async def attack(self):
+        stalkers = self.units(UnitTypeId.STALKER).ready.idle
+
+        for stalker in stalkers:
+            if self.units(UnitTypeId.STALKER).amount > 5:
+                stalker.attack(self.enemy_start_location[0])
 
 
     def on_end(self, result):
-        print("Game finish")
+        print("Game finish", result)
         # Do things here after the game ends
